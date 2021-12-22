d275e34468800576f978a1d272ee57d160a86c2e
Mahdieh Ebrahimi <evrusebr@gmail.com>
2021-12-22 13:09:21 +0330

    Update HKPBC.h
    again I changed the whole file!

--- HKPBC.h
+++ HKPBC.h
@@ -1,7 +1,8 @@
-#ifndef __HKPBC_H
-#define __HKPBC_H
+#ifndef __HKPBC_Serial_H
+#define __HKPBC_Serial_H
 #include <vector>
 
+using namespace std;
 
 const unsigned SIZE = 103968;
 
@@ -10,9 +11,12 @@ class HKPBC
 public:
 	int Lx;
 	int Ly;
-	int size;
+	int num;
 	int* labels;
+	int* labels_g;
 	int  n_labels = 0; 
+	int  n_labels_g = 0; 
+	int size, rank, last_label;
 
 
 public:
@@ -23,17 +27,26 @@ public:
 	
 	int uf_find(int x);
 	
-	void uf_pbc(std::vector<int> &matrix);
+	// void uf_pbc(vector<int> &matrix);
 	
 	int uf_union(int x, int y);
 
-	int uf_make_set(void);
+	int uf_make_set(void); //void
 
-	void uf_initialize(int max_labels); 
+	void uf_initialize(int max_labels);
 
 	void uf_done(void); 
 
-	std::vector<int> HK(std::vector<int> &matrix);
+	int HK(vector<int> &matrix, int last_label);
+
+	vector<int> serial(vector<vector<int>> &niegh, vector<vector<int>> &total, int max_lab);
+
+	void Hks_pbc(vector<int> &A, vector<int> &B, vector<vector<int>> &total, int j, int k);
+
+	void pbc_union(vector<vector<int>> &total, int newlabel, int oldlabel);
+
+	// int uf_find_g(int x, std::vector<int> &matrix);
+
 	
 };
 #endif
